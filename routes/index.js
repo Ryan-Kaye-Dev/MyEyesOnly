24377d93e4b2102f93d379a2e8a3dd7f6a74570f
Ryan Kaye <ryan@Ryans-MBP.lan>
2024-01-21 22:20:28 +0000

    Added routes for sign up and login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const signupController = require("../controllers/signupController");
+const loginController = require("../controllers/loginController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -12,8 +14,12 @@ router.get("/sign-up", function (req, res, next) {
 });
 
 //POST Sign Up page
-router.post("/sign-up", function (req, res, next) {
-  res.redirect("/");
-});
+router.post("/sign-up", signupController.signup_post);
+
+//GET Login page
+router.get("/log-in", loginController.login_get);
+
+//POST Login page
+router.post("/log-in", loginController.login_post);
 
 module.exports = router;
